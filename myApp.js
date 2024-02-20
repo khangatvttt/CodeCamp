30b063d5cce1a776f2f27d00f97874999a8dd513
Khang <khangatvttt@gmail.com>
2024-02-20 11:40:33 +0700

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -1,1 +1,40 @@
-console.log("Hello World")
+let express = require('express');
+let app = express();
+console.log("Hello World");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
